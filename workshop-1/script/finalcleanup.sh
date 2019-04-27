a9c4216737431fa46850c7c4c674959470dcfa1e
Kamran Habib <kmhabib@gmail.com>
2019-04-27 14:50:17 +1000

    added finalcleanup.sh script

---
+++ workshop-1/script/finalcleanup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Fill the role for nodegroup. looks like eksctl-mythicalmysfits-nodegroup-NodeInstanceRole-1TLRKSZC5S36X
+ROLE='eksctl-mythicalmysfits-nodegroup-NodeInstanceRole-1TLRKSZC5S36X'
+
+aws iam delete-role-policy --role-name EKSDeepDiveCodeBuildKubectlRole --policy-name eks-describe
+kubectl delete deployments hello-k8s
+kubectl delete services hello-k8s
+kubectl delete -f /sydummit-eksworkshop-2019/Kubernetes/mono/monolith-app.yaml
+kubectl delete -f /sydummit-eksworkshop-2019/Kubernetes/micro/likeservice-app.yaml
+kubectl delete -f /sydummit-eksworkshop-2019/Kubernetes/micro/nolikeservice-app.yaml
+kubectl delete -f /sydummit-eksworkshop-2019/Kubernetes/micro/mythical-ingress.yaml
+kubectl delete -f /sydummit-eksworkshop-2019/Kubernetes/micro/alb-ingress-controller.yaml
+kubectl delete -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.0.0/docs/examples/rbac-role.yaml
+kubectl delete -f /home/ec2-user/environment/sydummit-eksworkshop-2019/workshop-1/Lab4/fluentd.yml
+kubectl delete -f /home/ec2-user/environment/sydummit-eksworkshop-2019/Kubernetes/micro/likeservice-app.yaml 
+kubectl delete -f /home/ec2-user/environment/sydummit-eksworkshop-2019/Kubernetes/micro/nolikeservice-app.yaml
+kubectl delete -f /home/ec2-user/environment/sydummit-eksworkshop-2019/Kubernetes/micro/mythical-ingress.yaml 
+
+
+aws iam delete-role-policy --role-name $ROLE --policy-name Logs-Policy-For-Worker
+aws iam delete-role-policy --role-name $ROLE --policy-name ingress-db
+
+aws eks delete-cluster --name mythicalmysfits
+
+
