5b3908094b642aee2a54148757e9ca3b8b2146ab
Kamran Habib <kmhabib@gmail.com>
2019-04-27 15:08:51 +1000

    added finalcleanup.sh script

--- workshop-1/script/finalcleanup.sh
+++ workshop-1/script/finalcleanup.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 # Fill the role for nodegroup. looks like eksctl-mythicalmysfits-nodegroup-NodeInstanceRole-1TLRKSZC5S36X
-ROLE='eksctl-mythicalmysfits-nodegroup-NodeInstanceRole-1TLRKSZC5S36X'
+ROLE='YOUR_ROLENAME_FOR_EKS_NODEGROUP'
 
 aws iam delete-role-policy --role-name EKSDeepDiveCodeBuildKubectlRole --policy-name eks-describe
+aws iam delete-role --role-name EKSDeepDiveCodeBuildKubectlRole
 kubectl delete deployments hello-k8s
 kubectl delete services hello-k8s
-kubectl delete -f /sydummit-eksworkshop-2019/Kubernetes/mono/monolith-app.yaml
-kubectl delete -f /sydummit-eksworkshop-2019/Kubernetes/micro/likeservice-app.yaml
-kubectl delete -f /sydummit-eksworkshop-2019/Kubernetes/micro/nolikeservice-app.yaml
-kubectl delete -f /sydummit-eksworkshop-2019/Kubernetes/micro/mythical-ingress.yaml
-kubectl delete -f /sydummit-eksworkshop-2019/Kubernetes/micro/alb-ingress-controller.yaml
+kubectl delete -f /home/ec2-user/environment/sydummit-eksworkshop-2019/Kubernetes/mono/monolith-app.yaml
+kubectl delete -f /home/ec2-user/environment/sydummit-eksworkshop-2019/Kubernetes/micro/likeservice-app.yaml
+kubectl delete -f /home/ec2-user/environment/sydummit-eksworkshop-2019/Kubernetes/micro/nolikeservice-app.yaml
+kubectl delete -f /home/ec2-user/environment/sydummit-eksworkshop-2019/Kubernetes/micro/mythical-ingress.yaml
+kubectl delete -f /home/ec2-user/environment/sydummit-eksworkshop-2019/Kubernetes/micro/alb-ingress-controller.yaml
 kubectl delete -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.0.0/docs/examples/rbac-role.yaml
 kubectl delete -f /home/ec2-user/environment/sydummit-eksworkshop-2019/workshop-1/Lab4/fluentd.yml
 kubectl delete -f /home/ec2-user/environment/sydummit-eksworkshop-2019/Kubernetes/micro/likeservice-app.yaml 
@@ -19,8 +20,8 @@ kubectl delete -f /home/ec2-user/environment/sydummit-eksworkshop-2019/Kubernete
 
 
 aws iam delete-role-policy --role-name $ROLE --policy-name Logs-Policy-For-Worker
-aws iam delete-role-policy --role-name $ROLE --policy-name ingress-db
+aws iam delete-role-policy --role-name $ROLE --policy-name ingress-ddb
 
-aws eks delete-cluster --name mythicalmysfits
+eksctl delete cluster --name=mythicalmysfits
 
 
